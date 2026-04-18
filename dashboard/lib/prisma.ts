63369771f298fadd3dd25ec1a688084bd412906d
NarenS31 <narens831709@gmail.com>
2026-04-18 11:39:01 -0400

    Fix Vercel deployment: fix prisma import, upgrade Next.js to 14.2.35

--- dashboard/lib/prisma.ts
+++ dashboard/lib/prisma.ts
@@ -1,13 +1,4 @@
-import { PrismaClient } from "../../node_modules/@prisma/client";
-import fs from "node:fs";
-import path from "node:path";
-
-if (!process.env.PRISMA_QUERY_ENGINE_LIBRARY) {
-	const enginePath = path.resolve(process.cwd(), "../node_modules/.prisma/client/libquery_engine-darwin-arm64.dylib.node");
-	if (fs.existsSync(enginePath)) {
-		process.env.PRISMA_QUERY_ENGINE_LIBRARY = enginePath;
-	}
-}
+import { PrismaClient } from "@prisma/client";
 
 declare global {
 	// eslint-disable-next-line no-var
